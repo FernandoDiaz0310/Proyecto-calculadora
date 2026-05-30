b02c1bbe64a8e950b8bb4be8893cae1f8aa2c65b
Fernando Tomas Diaz Garcia <fernando_tomas2@ucol.mx>
2026-05-30 11:52:01 -0600

    Se agrego la funcionalidad de multiplicar y dividir

--- Proyecto-calculadora/calculadora.py
+++ Proyecto-calculadora/calculadora.py
@@ -3,8 +3,11 @@ print("          CALCULADORA BÁSICA       ")
 print("===================================")
 print("1) Sumar")
 print("2) Restar")
-op = input("\nSeleccione una opción (1/2): ").strip()
-if op not in ("1", "2"):
+print("3) Multiplicar")
+print("4) Dividir")
+print(" ===================================")
+op = input("\nSeleccione una opción (1/2/3/4): ").strip()
+if op not in ("1", "2", "3", "4"):
     print("Opción inválida. Fin.")
     exit()
 
@@ -21,7 +24,20 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: No se puede dividir por cero"
+    return a / b
+
 if op == "1":
     print(f"\nSuma: {sumar(num1, num2)}")
-else:
+elif op == "2":
     print(f"\nResta: {restar(num1, num2)}")
+elif op == "3":
+    print(f"\nMultiplicación: {multiplicar(num1, num2)}")
+else:
+    resultado = dividir(num1, num2)
+    print(f"\nDivisión: {resultado}")
